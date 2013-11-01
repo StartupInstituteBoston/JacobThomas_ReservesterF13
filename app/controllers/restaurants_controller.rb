7d93aee760f8271157d5d97f6d11e396fc515bc0
jakthom <jacobthomas1989@gmail.com>
2013-10-31 20:14:28 -0400

    Restaurants controller, correct paths, basic view content

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,21 @@
 class RestaurantsController < ApplicationController
 
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
   end
 
   def create 
+    @restaurant = Restaurant.new(params[:restaurant].permit(:name, :address, :phone, :description))
+
+    @restaurant.save
+    redirect_to @restaurant
   end
 
   def edit
@@ -20,5 +26,10 @@ class RestaurantsController < ApplicationController
 
   def destroy
   end 
+
+    private 
+      def post_params
+        params.require(:restaurant).permit(:name, :address, :phone, :description)
+      end
 
 end
