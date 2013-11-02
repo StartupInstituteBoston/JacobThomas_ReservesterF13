00c57ca4e1da3d0dc894e9574054e17790f2482f
jakthom <jacobthomas1989@gmail.com>
2013-11-01 23:35:45 -0400

    Bootstrap

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class RestaurantsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save restaurant without title" do 
+    restaurant = Restaurant.new
+    assert !restaurant.save, "Saved the post without a title"
+  end
 end
