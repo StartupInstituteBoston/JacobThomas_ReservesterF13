6f460d5df670660d5b628cf396412e03bba28da2
jakthom <jacobthomas1989@gmail.com>
2013-11-08 17:46:39 -0500

    Added name_to_owners migration, added name field to signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131108122807) do
+ActiveRecord::Schema.define(version: 20131108223415) do
 
   create_table "owners", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20131108122807) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "owners", ["email"], name: "index_owners_on_email", unique: true, using: :btree
