87e69f88df8ce5e7728c1b5486c1a5f7ed61226a
jakthom <jacobthomas1989@gmail.com>
2013-11-20 23:06:27 -0500

    Finished owner dashboard

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,6 +1,9 @@
 class OwnersController < ApplicationController
 
+  before_filter :authenticate_owner!
+
   def dashboard 
+    @restaurants = Restaurant.find(:all, :conditions => { :owner_id => current_owner.id })
   end
 
 
